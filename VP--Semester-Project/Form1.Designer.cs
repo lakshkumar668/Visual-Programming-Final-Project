cca5b79fd06b72ec7c41f3b6b178ed2d9925e302
lakshkumar668 <60233787+lakshkumar668@users.noreply.github.com>
2022-12-17 22:38:17 +0500

    Complete Game Code Commit

---
+++ VP--Semester-Project/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace VP__Semester_Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pipeTop = new System.Windows.Forms.PictureBox();
+            this.flappyBird = new System.Windows.Forms.PictureBox();
+            this.pipeButtom = new System.Windows.Forms.PictureBox();
+            this.pipeGround = new System.Windows.Forms.PictureBox();
+            this.scoreText = new System.Windows.Forms.Label();
+            this.gametimer = new System.Windows.Forms.Timer(this.components);
+            this.pipeTop1 = new System.Windows.Forms.PictureBox();
+            this.pipeButtom1 = new System.Windows.Forms.PictureBox();
+            this.pipeTop2 = new System.Windows.Forms.PictureBox();
+            this.pipeButtom2 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.restartToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeTop)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.flappyBird)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeButtom)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeGround)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeTop1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeButtom1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeTop2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeButtom2)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pipeTop
+            // 
+            this.pipeTop.Image = ((System.Drawing.Image)(resources.GetObject("pipeTop.Image")));
+            this.pipeTop.Location = new System.Drawing.Point(280, -28);
+            this.pipeTop.Name = "pipeTop";
+            this.pipeTop.Size = new System.Drawing.Size(61, 138);
+            this.pipeTop.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pipeTop.TabIndex = 0;
+            this.pipeTop.TabStop = false;
+            // 
+            // flappyBird
+            // 
+            this.flappyBird.Image = ((System.Drawing.Image)(resources.GetObject("flappyBird.Image")));
+            this.flappyBird.Location = new System.Drawing.Point(27, 138);
+            this.flappyBird.Name = "flappyBird";
+            this.flappyBird.Size = new System.Drawing.Size(45, 41);
+            this.flappyBird.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.flappyBird.TabIndex = 1;
+            this.flappyBird.TabStop = false;
+            // 
+            // pipeButtom
+            // 
+            this.pipeButtom.Image = ((System.Drawing.Image)(resources.GetObject("pipeButtom.Image")));
+            this.pipeButtom.Location = new System.Drawing.Point(280, 256);
+            this.pipeButtom.Name = "pipeButtom";
+            this.pipeButtom.Size = new System.Drawing.Size(61, 200);
+            this.pipeButtom.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pipeButtom.TabIndex = 2;
+            this.pipeButtom.TabStop = false;
+            // 
+            // pipeGround
+            // 
+            this.pipeGround.Image = ((System.Drawing.Image)(resources.GetObject("pipeGround.Image")));
+            this.pipeGround.Location = new System.Drawing.Point(-1, 398);
+            this.pipeGround.Name = "pipeGround";
+            this.pipeGround.Size = new System.Drawing.Size(999, 65);
+            this.pipeGround.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pipeGround.TabIndex = 3;
+            this.pipeGround.TabStop = false;
+            // 
+            // scoreText
+            // 
+            this.scoreText.AutoSize = true;
+            this.scoreText.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scoreText.Location = new System.Drawing.Point(215, 430);
+            this.scoreText.Name = "scoreText";
+            this.scoreText.Size = new System.Drawing.Size(93, 22);
+            this.scoreText.TabIndex = 4;
+            this.scoreText.Text = "Score : 0";
+            // 
+            // gametimer
+            // 
+            this.gametimer.Enabled = true;
+            this.gametimer.Interval = 20;
+            this.gametimer.Tick += new System.EventHandler(this.gameTimerEvent);
+            // 
+            // pipeTop1
+            // 
+            this.pipeTop1.Image = ((System.Drawing.Image)(resources.GetObject("pipeTop1.Image")));
+            this.pipeTop1.Location = new System.Drawing.Point(529, -53);
+            this.pipeTop1.Name = "pipeTop1";
+            this.pipeTop1.Size = new System.Drawing.Size(61, 180);
+            this.pipeTop1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pipeTop1.TabIndex = 5;
+            this.pipeTop1.TabStop = false;
+            // 
+            // pipeButtom1
+            // 
+            this.pipeButtom1.Image = ((System.Drawing.Image)(resources.GetObject("pipeButtom1.Image")));
+            this.pipeButtom1.Location = new System.Drawing.Point(529, 290);
+            this.pipeButtom1.Name = "pipeButtom1";
+            this.pipeButtom1.Size = new System.Drawing.Size(61, 207);
+            this.pipeButtom1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pipeButtom1.TabIndex = 6;
+            this.pipeButtom1.TabStop = false;
+            // 
+            // pipeTop2
+            // 
+            this.pipeTop2.Image = ((System.Drawing.Image)(resources.GetObject("pipeTop2.Image")));
+            this.pipeTop2.Location = new System.Drawing.Point(805, -10);
+            this.pipeTop2.Name = "pipeTop2";
+            this.pipeTop2.Size = new System.Drawing.Size(61, 95);
+            this.pipeTop2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pipeTop2.TabIndex = 7;
+            this.pipeTop2.TabStop = false;
+            // 
+            // pipeButtom2
+            // 
+            this.pipeButtom2.Image = ((System.Drawing.Image)(resources.GetObject("pipeButtom2.Image")));
+            this.pipeButtom2.Location = new System.Drawing.Point(805, 236);
+            this.pipeButtom2.Name = "pipeButtom2";
+            this.pipeButtom2.Size = new System.Drawing.Size(61, 213);
+            this.pipeButtom2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pipeButtom2.TabIndex = 8;
+            this.pipeButtom2.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.restartToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(534, 24);
+            this.menuStrip1.TabIndex = 9;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // restartToolStripMenuItem
+            // 
+            this.restartToolStripMenuItem.Name = "restartToolStripMenuItem";
+            this.restartToolStripMenuItem.Size = new System.Drawing.Size(55, 20);
+            this.restartToolStripMenuItem.Text = "Restart";
+            this.restartToolStripMenuItem.Click += new System.EventHandler(this.restartToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Aqua;
+            this.ClientSize = new System.Drawing.Size(534, 461);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.scoreText);
+            this.Controls.Add(this.pipeGround);
+            this.Controls.Add(this.pipeButtom);
+            this.Controls.Add(this.pipeTop);
+            this.Controls.Add(this.flappyBird);
+            this.Controls.Add(this.pipeButtom1);
+            this.Controls.Add(this.pipeTop1);
+            this.Controls.Add(this.pipeTop2);
+            this.Controls.Add(this.pipeButtom2);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Flappy Bird Game";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pipeTop)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.flappyBird)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeButtom)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeGround)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeTop1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeButtom1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeTop2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeButtom2)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pipeTop;
+        private System.Windows.Forms.PictureBox flappyBird;
+        private System.Windows.Forms.PictureBox pipeButtom;
+        private System.Windows.Forms.PictureBox pipeGround;
+        private System.Windows.Forms.Label scoreText;
+        private System.Windows.Forms.Timer gametimer;
+        private System.Windows.Forms.PictureBox pipeTop1;
+        private System.Windows.Forms.PictureBox pipeButtom1;
+        private System.Windows.Forms.PictureBox pipeTop2;
+        private System.Windows.Forms.PictureBox pipeButtom2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem restartToolStripMenuItem;
+    }
+}
+
---
+++ VP--Semester-Project/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VP__Semester_Project
+{
+    public partial class Form1 : Form
+    {
+        int Speed = 3;
+        int gravity = 3;
+        int score = 0;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void gameTimerEvent(object sender, EventArgs e)
+        {
+            flappyBird.Top += gravity;
+            pipeButtom.Left -= Speed;
+            pipeTop.Left -= Speed;
+            pipeButtom1.Left -= Speed;
+            pipeTop1.Left -= Speed;
+            pipeButtom2.Left -= Speed;
+            pipeTop2.Left -= Speed;
+            scoreText.Text = "Score : " + score;
+
+            if (pipeButtom.Left < -28 || pipeTop.Left < -28 )
+            {
+                addPipe();
+            }
+           else if (pipeButtom1.Left < -28 || pipeTop1.Left < -28)
+            {
+                addPipe1();
+            }
+            else if (pipeButtom2.Left < -28 || pipeTop2.Left < -28)
+            {
+                addPipe2();
+            }
+
+            if (flappyBird.Bounds.IntersectsWith(pipeButtom.Bounds) || flappyBird.Bounds.IntersectsWith(pipeTop.Bounds) || flappyBird.Bounds.IntersectsWith(pipeGround.Bounds) || flappyBird.Top < -25)
+            {
+                Draw();
+            }
+
+            if (flappyBird.Bounds.IntersectsWith(pipeButtom1.Bounds) || flappyBird.Bounds.IntersectsWith(pipeTop1.Bounds) || flappyBird.Bounds.IntersectsWith(pipeGround.Bounds) || flappyBird.Top < -25)
+            {
+                Draw();
+            }
+            if (flappyBird.Bounds.IntersectsWith(pipeButtom2.Bounds) || flappyBird.Bounds.IntersectsWith(pipeTop2.Bounds) || flappyBird.Bounds.IntersectsWith(pipeGround.Bounds) || flappyBird.Top < -25)
+            {
+                Draw();
+            }
+            
+        }
+
+        private void keyDown(object sender, KeyEventArgs e)
+        {
+
+            if (e.KeyCode == Keys.Space)
+            {
+                gravity = -5;
+            }
+        }
+
+        private void keyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                gravity = 5;
+            }
+        }
+        public void Draw()
+        {
+            gametimer.Stop();
+            MessageBox.Show("GameOver" +"\n" + "\n" + "your Socre is : "+ score);
+        }
+        public void addPipe()
+        {
+            pipeButtom.Left = 750;
+            pipeTop.Left = 750;
+            score++;
+        }
+        public void addPipe1()
+        {
+            pipeButtom1.Left = 750;
+            pipeTop1.Left = 750;
+            score++;
+        }
+        public void addPipe2()
+        {
+            pipeButtom2.Left = 750;
+            pipeTop2.Left = 750;
+            score++;
+        }
+
+        private void restartToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form1 obj = new Form1();
+            obj.ShowDialog();
+
+        }
+
+        
+    }
+}
